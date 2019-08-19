987d739697cb7c81619f9f7079444c7c4ace12b4
Yann Zaoui <yzaoui7@gmail.com>
2019-08-19 02:51:20 -0400

    Flesh out log in page

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { FormEventHandler } from "react";
+import { Link } from "react-router-dom";
+import "./LoginRegisterPage.css";
 
 const LoginPage: React.FC = () => {
-    return <h1>Log in</h1>;
+    return <>
+        <form onSubmit={handleSubmit} className="LoginRegisterPage-form">
+            <h1>Log in to PBBG</h1>
+            <input type="text" required placeholder="Username" autoComplete="username" />
+            <input type="password" required placeholder="Password" autoComplete="current-password" />
+            <button type="submit">Log in</button>
+        </form>
+        <Link to="/register" className="LoginRegisterPage-login-link">New user? Register</Link>
+    </>;
+};
+
+const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    console.log("submitted login form");
 };
 
 export default LoginPage;
--- src/RegisterPage.tsx
+++ src/RegisterPage.tsx
@@ -1,16 +1,16 @@
 import React, { FormEventHandler } from "react";
 import { Link } from "react-router-dom";
-import "./RegisterPage.css";
+import "./LoginRegisterPage.css";
 
 const RegisterPage: React.FC = () => {
     return <>
-        <form onSubmit={handleSubmit} className="RegisterPage-form">
+        <form onSubmit={handleSubmit} className="LoginRegisterPage-form">
             <h1>Register your account</h1>
             <input type="text" required placeholder="Username" autoComplete="username" />
             <input type="password" required placeholder="Password" autoComplete="new-password" />
             <button type="submit">Register</button>
         </form>
-        <Link to="/login" className="RegisterPage-login-link">Existing user? Log in</Link>
+        <Link to="/login" className="LoginRegisterPage-login-link">Existing user? Log in</Link>
     </>;
 };
 
