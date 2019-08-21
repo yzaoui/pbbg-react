603e3a64592628161cb568fa45e4b7a3775152d0
Yann Zaoui <yzaoui7@gmail.com>
2019-08-21 12:21:20 -0400

    Add home link to sidebar nav

--- src/MemberNav.tsx
+++ src/MemberNav.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "./common.css";
+import { NavLink } from "react-router-dom";
 
 const MemberNav: React.FC = () => {
     return <nav className="sidebar">
-        Member!
+        <div className="username">USERNAME</div>
+        <div className="navigation">
+            <NavLink to="/" className="sidebar-item" activeClassName="current-site-section">
+                <span className="sidebar-item-icon" role="img" aria-label="Home">🏠</span>
+                <span>Home</span>
+            </NavLink>
+        </div>
     </nav>;
 };
 
