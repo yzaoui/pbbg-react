304635c301961d43e3dfff68658e712202840b78
Yann Zaoui <yzaoui7@gmail.com>
2019-08-21 12:33:04 -0400

    Add & wire up logout button on member nav

--- src/MemberNav.tsx
+++ src/MemberNav.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./common.css";
 import { NavLink } from "react-router-dom";
+import authenticationService from "./authentication.service";
 
 const MemberNav: React.FC = () => {
     return <nav className="sidebar">
@@ -11,7 +12,12 @@ const MemberNav: React.FC = () => {
                 <span>Home</span>
             </NavLink>
         </div>
+        <div className="sidebar-logout">
+            <button onClick={handleLogoutClick}>Log out</button>
+        </div>
     </nav>;
 };
+
+const handleLogoutClick = () => authenticationService.logout();
 
 export default MemberNav;
