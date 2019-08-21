8416e94f052cdf854795dfb1cfbe49fef6021f42
Yann Zaoui <yzaoui7@gmail.com>
2019-08-21 00:13:47 -0400

    Add skeleton member nav

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import "normalize.css"
 import "./common.css"
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import GuestRoute from "./GuestRoute";
 import IndexGuestPage from "./page/IndexGuestPage";
+import IndexMemberPage from "./page/IndexMemberPage";
 import RegisterPage from "./page/RegisterPage";
 import LoginPage from "./page/LoginPage";
 import GuestNav from "./GuestNav";
+import MemberNav from "./MemberNav";
 import authenticationService from "./authentication.service";
-import IndexMemberPage from "./page/IndexMemberPage";
-import GuestRoute from "./GuestRoute";
 
 interface State {
     currentUserToken: string | null;
@@ -29,7 +30,7 @@ class App extends React.Component<{}, State> {
 
         return <Router>
             <div className="container">
-                <GuestNav />
+                {isLoggedIn ? <MemberNav /> : <GuestNav />}
                 <main>
                     <Route path="/" exact render={() => isLoggedIn ? <IndexMemberPage /> : <IndexGuestPage />} />
                     <GuestRoute path="/register" component={RegisterPage} />
---
+++ src/MemberNav.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./common.css";
+
+const MemberNav: React.FC = () => {
+    return <nav className="sidebar">
+        Member!
+    </nav>;
+};
+
+export default MemberNav;
