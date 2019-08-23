b9e5b1142ff3c00fcbb4b71be71daca1df93a578
Yann Zaoui <yzaoui7@gmail.com>
2019-08-23 00:28:17 -0400

    Implement MemberRoute

---
+++ src/MemberRoute.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { Redirect, Route, RouteProps } from "react-router-dom"
+import authenticationService from "./authentication.service"
+
+/**
+ * Wrapper around React.Route which redirects a guest user to "/login", passing in the target destination.
+ */
+const MemberRoute: React.FC<RouteProps> = ({ component: Component, ...rest }) => {
+    if (!Component) return null;
+    return <Route
+        {...rest}
+        render={props =>
+            authenticationService.currentUserValue !== null ? <Component {...props} /> : <Redirect to={{ pathname: "/login", state: { from: props.location } }} />
+        }
+    />;
+};
+
+export default MemberRoute;
