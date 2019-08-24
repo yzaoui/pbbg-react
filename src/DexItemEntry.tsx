8c6f466d9a27da0f0493eabad8cc76e6c945c3a0
Yann Zaoui <yzaoui7@gmail.com>
2019-08-23 23:29:38 -0400

    Split dex subpage CSS

--- src/DexItemEntry.tsx
+++ src/DexItemEntry.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ItemEnum } from "./backend/dex";
+import "./DexEntry.css"
 
 interface Props {
     id: string;
     item: ItemEnum;
 }
 
-const DexItemEntry: React.FC<Props> = ({ id, item }) => <li>
-    <Link to={"/dex/items/" + id}>
+const DexItemEntry: React.FC<Props> = ({ id, item }) => <li className="entry-li">
+    <Link to={"/dex/items/" + id} className="entry-container">
         <span>#{id}</span>
         <img src={item.imgURL} alt={item.friendlyName + " sprite"} />
         <span>{item.friendlyName}</span>
--- src/DexUnitEntry.tsx
+++ src/DexUnitEntry.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { MyUnitEnum } from "./backend/dex";
+import "./DexEntry.css"
 
 interface Props {
     id: string;
     unit: MyUnitEnum;
 }
 
-const DexUnitEntry: React.FC<Props> = ({ id, unit }) => <li>
-    <Link to={"/dex/units/" + id}>
+const DexUnitEntry: React.FC<Props> = ({ id, unit }) => <li className="entry-li">
+    <Link to={"/dex/units/" + id} className="entry-container unit">
         <span>#{id}</span>
         <img src={unit.iconURL} alt={unit.friendlyName + " sprite"} />
         <span>{unit.friendlyName}</span>
