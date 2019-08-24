242421a8f3054e1d6d1eccd8e3d87fe964d7ea55
Yann Zaoui <yzaoui7@gmail.com>
2019-08-24 15:28:15 -0400

    Add loading to squad page

--- src/page/SquadPage.tsx
+++ src/page/SquadPage.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Squad } from "../backend/squad";
+import squadService from "../backend/squad.service";
+import { Subscription } from "rxjs";
+import LoadingSpinner from "../component/LoadingSpinner";
 
-const SquadPage: React.FC = () => <>
-    [WIP] squad page
-</>;
+interface State {
+    state: "loading" | "error" | Squad;
+}
+
+class SquadPage extends React.Component<{}, State> {
+    readonly state: Readonly<State> = {
+        state: "loading"
+    };
+
+    request?: Subscription;
+
+    componentDidMount() {
+        this.request = squadService.getSquad()
+            .subscribe(
+                res => this.setState({ state: res.data }),
+                error => this.setState({ state: "error" })
+            )
+    }
+
+    componentWillUnmount() {
+        this.request && this.request.unsubscribe();
+    }
+
+    render() {
+        if (this.state.state === "loading") return <LoadingSpinner />;
+        else if (this.state.state === "error") return "ERROR";
+
+        return <>
+            <button className="fancy" style={{ alignSelf: "center" }}>Heal Squad</button>
+        </>;
+    }
+}
 
 export default SquadPage;
