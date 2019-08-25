2e7679df9bb6fc96b481b06031e9ee78891f0bd4
Yann Zaoui <yzaoui7@gmail.com>
2019-08-25 12:13:09 -0400

    Add loading state to healing squad button

--- src/page/SquadPage.tsx
+++ src/page/SquadPage.tsx
@@ -4,14 +4,17 @@ import squadService from "../backend/squad.service";
 import { Subscription } from "rxjs";
 import LoadingSpinner from "../component/LoadingSpinner";
 import PBBGUnit from "../component/PBBGUnit";
+import classNames from "classnames";
 
 interface State {
     state: "loading" | "error" | Squad;
+    healing: boolean;
 }
 
 class SquadPage extends React.Component<{}, State> {
     readonly state: Readonly<State> = {
-        state: "loading"
+        state: "loading",
+        healing: false
     };
 
     request?: Subscription;
@@ -32,15 +35,24 @@ class SquadPage extends React.Component<{}, State> {
         else if (this.state.state === "error") return "ERROR";
 
         return <>
-            <button className="fancy" style={{ alignSelf: "center" }} onClick={this.handleHealClick}>Heal Squad</button>
+            <button
+                className={classNames("fancy", { "loading": this.state.healing })}
+                style={{ alignSelf: "center" }}
+                onClick={this.handleHealClick}
+                disabled={this.state.healing}
+            >
+                Heal Squad{this.state.healing ? " (Loading…)" : ""}
+            </button>
             {this.state.state.units.map(unit => <PBBGUnit key={unit.id} unit={unit} style={{ margin: "4px" }} />)}
         </>;
     }
 
     handleHealClick = () => {
+        this.setState({ healing: true });
+
         this.request = squadService.healSquad().subscribe(
-            res => this.setState({ state: res.data }),
-            error => this.setState({ state: "error" })
+            res => this.setState({ healing: false, state: res.data }),
+            error => this.setState({ healing: false, state: "error" })
         );
     };
 }
