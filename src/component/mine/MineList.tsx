8cf82e1e503eb1ddb1798d9836a4a2097553d8bd
Yann Zaoui <yzaoui7@gmail.com>
2019-08-26 16:27:06 -0400

    Add mine list row components to mine list

--- src/component/mine/MineList.tsx
+++ src/component/mine/MineList.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import "./MineList.css";
 import LoadingSpinner from "../LoadingSpinner";
 import { MineTypeList } from "../../backend/mine";
+import MineListRow from "./MineListRow";
+import MineListUnknownRow from "./MineListUnknownRow";
 
 interface Props {
-    state: "loading" | "error" | MineTypeList
+    state: "loading" | "error" | MineTypeList;
 }
 
 const MineList: React.FC<Props> = ({ state }) => <table className="MineList">
@@ -12,17 +14,24 @@ const MineList: React.FC<Props> = ({ state }) => <table className="MineList">
         <tr>
             <th>Mine name</th>
             <th>Minimum lvl.</th>
-            <th>Generate mine</th>
+            <th>Enter mine</th>
         </tr>
     </thead>
     <tbody>
-    {state === "loading" &&
-        <tr>
+    {(() => {
+        if (state === "loading") return <tr>
             <td colSpan={3}>
                 <div style={{ display: "flex", justifyContent: "center" }}><LoadingSpinner /></div>
             </td>
-        </tr>
-    }
+        </tr>;
+
+        else if (state === "error") return "ERROR";
+
+        return <>
+            {state.types.map(mineType => <MineListRow mineType={mineType} />)}
+            {state.nextUnlockLevel !== undefined && <MineListUnknownRow nextUnlockLevel={state.nextUnlockLevel} />}
+        </>;
+    })()}
     </tbody>
 </table>;
 
