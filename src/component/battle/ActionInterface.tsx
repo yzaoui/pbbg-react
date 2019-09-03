1a6a3853e22acf53080578b2cebc2f7bedb4b1cc
Yann Zaoui <yzaoui7@gmail.com>
2019-09-03 18:37:47 -0400

    Improve style of battle ActionInterface

--- src/component/battle/ActionInterface.tsx
+++ src/component/battle/ActionInterface.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import LoadingButton from "../LoadingButton";
+import "./ActionInterface.css";
 
 type Props = {
     performingAction: boolean;
@@ -11,12 +12,15 @@ type Props = {
     onProcessAllyTurn: () => void;
 });
 
-const ActionInterface: React.FC<Props> = (props) => <div>
-    {props.enemyTurn ?
-        <LoadingButton loading={props.performingAction} onClick={props.onProcessEnemyTurn}>Process enemy turn</LoadingButton>
-    :
-        <LoadingButton loading={props.performingAction} onClick={props.onProcessAllyTurn}>Attack</LoadingButton>
-    }
+const ActionInterface: React.FC<Props> = (props) => <div className="ActionInterface">
+    <h1>Actions</h1>
+    <div>
+        {props.enemyTurn ?
+            <LoadingButton loading={props.performingAction} onClick={props.onProcessEnemyTurn}>Process enemy turn</LoadingButton>
+            :
+            <LoadingButton loading={props.performingAction} onClick={props.onProcessAllyTurn}>Attack</LoadingButton>
+        }
+    </div>
 </div>;
 
 export default ActionInterface;
